f862116abc75b951576296913bdc568bc42b813b
Geoff Irwin <geoff.irwin@Marine.ie>
2019-02-19 15:44:31 +0000

    basic working

--- src/index.js
+++ src/index.js
@@ -3,10 +3,90 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+import Autosuggest from 'react-autosuggest';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const getSuggestionValue = suggestion => suggestion.vesselName;
+
+// Use your imagination to render suggestions.
+const renderSuggestion = suggestion => (
+  <div>
+    Vessel Name:{suggestion.VesselName}<p>{suggestion.cfr}</p>
+  </div>
+);
+
+class Example extends React.Component {
+  constructor() {
+    super();
+
+    // Autosuggest is a controlled component.
+    // This means that you need to provide an input value
+    // and an onChange handler that updates this value (see below).
+    // Suggestions also need to be provided to the Autosuggest,
+    // and they are initially empty because the Autosuggest is closed.
+    this.state = {
+      value: '',
+      suggestions: []
+    };
+  }
+
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+
+  // Autosuggest will call this function every time you need to update suggestions.
+  // You already implemented this logic above, so just use it.
+  onSuggestionsFetchRequested = ({ value }) => {
+    axios
+      .post('http://10.11.1.70:9200/allvessels/allevents/_search', {
+          "query": {
+            "match": {
+              "VesselName.edgengram": value
+            }
+          }
+      })
+      .then(res => {
+        const results = res.data.hits.hits.map(h => h._source)
+        this.setState({ suggestions: results })
+      })
+  }
+
+  // Autosuggest will call this function every time you need to clear suggestions.
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  render() {
+    const { value, suggestions } = this.state;
+
+    // Autosuggest will pass through all these props to the input.
+    const inputProps = {
+      placeholder: 'Check Vessel Name',
+      value,
+      onChange: this.onChange
+    };
+
+    // Finally, render it!
+    return (
+      <Autosuggest
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps}
+      />
+    );
+  }
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<Example />, rootElement)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
